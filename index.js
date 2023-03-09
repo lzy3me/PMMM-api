45d09dc91482541b6af3bf75eb08c00fd64ed3ea
fluke.jessada <26509248+lzy3me@users.noreply.github.com>
2023-03-10 01:17:00 +0700

    fix path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000
 app.use(compression())
 app.use(helmet())
 
-app.get('/m5pmmm/qrpayment', (req, res) => {
+app.get('/qrpayment', (req, res) => {
   try {
     const { msid, amount } = req.query;
 
