61c0866c4b993cd38300e207d7304e31460ffc5e
F1uke_Jessada <8350713-fluke.jessada.s@users.noreply.gitlab.com>
2023-03-09 23:22:48 +0700

    added helmet and compression

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const generatePayload = require('promptpay-qr')
+const helmet = require('helmet')
+const compression = require('compression')
 const app = express()
 const port = 3000
 
+app.use(compression())
+app.use(helmet())
 
 app.get('/qrpayment', (req, res) => {
   const { msid, amount } = req.query;
@@ -13,5 +17,5 @@ app.get('/qrpayment', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
